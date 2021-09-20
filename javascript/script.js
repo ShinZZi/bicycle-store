b1fe05ad714c2a0e9ea98adfc6aa5cf3e16ff295
shinzzi <longnp1610@gmail.com>
2021-09-20 20:41:35 +0700

    header responsive

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,16 @@
 const menu_bars = document.querySelector('.fas.fa-bars');
+const menu_close = document.querySelector('.fas.fa-times');
+const nav_links = document.querySelector('.nav__links');
 console.log(menu_bars);
 
 menu_bars.addEventListener('click', () => {
-    menu_bars.classList.toggle('active');
+    menu_bars.classList.remove('active');
+    menu_close.classList.add('active');
+    nav_links.classList.add('active');
+})
+
+menu_close.addEventListener('click', () => {
+    menu_bars.classList.add('active');
+    menu_close.classList.remove('active');
+    nav_links.classList.remove('active');
 })
